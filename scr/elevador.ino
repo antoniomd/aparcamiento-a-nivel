b13418fbd0a4dca0d604b5c510bdd9daaed86515
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-06-11 16:56:11 +0200

    Update elevador.ino

---
+++ scr/elevador.ino
@@ -0,0 +1,35 @@
+/*
+Aparcamiento a nivel
+
+Este programa permite el control de un ascensor para un garaje en una casa.
+
+El programa permitira que un motor de c.c. controle el movimiento un ascensor.
+Mediante un botón en cada planta se llamará al ascensor y con el botón 0 o 1 situado en el ascensor se 
+seleccionará el piso donde ir. Unos finales de carrera permitirán conocer el piso donde se encuentra el 
+ascensor.
+
+El circuito:
+- botones.
+  Se usarán las entradas 3, 4, 5 y 6.
+
+- finales de carrera.
+  Se usarán las entradas 7 y 8.
+
+- Motor c.c.
+  Se usará la salida ¿?
+
+Created 11 de Junio de 2020
+By Antonio Molina Domínguez
+Modified 
+By
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
